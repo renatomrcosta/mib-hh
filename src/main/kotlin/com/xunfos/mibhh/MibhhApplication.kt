e9ed3bbed0a373a097ab1b62a3072344b2cb213f
Renato Costa <renato.rezende.costa@gmail.com>
2021-03-23 15:36:04 +0100

    initial commit

---
+++ src/main/kotlin/com/xunfos/mibhh/MibhhApplication.kt
@@ -0,0 +1,11 @@
+package com.xunfos.mibhh
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MibhhApplication
+
+fun main(args: Array<String>) {
+    runApplication<MibhhApplication>(*args)
+}
---
+++ src/test/kotlin/com/xunfos/mibhh/MibhhApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xunfos.mibhh
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MibhhApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
